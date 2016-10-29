fa9ece26e9c98b38f5aa7c48aa7aed7f043f2d08
federico123579 <federico123579@gmail.com>
2016-10-29 19:36:39 +0200

    Refined code and added new info

--- data_processor.py
+++ data_processor.py
@@ -10,16 +10,19 @@ if new_number_choice == "y":
     data = json.loads(response.read())
     price_str = json.dumps(data["extractorData"]["data"][0]["group"][0]["Price"][0]["text"])
     price = float(price_str[1:-7])
+    with open("data.txt") as f:
+        lines=f.readlines()
+        print lines[-1][11:-1]
 # Write Data
     now = datetime.datetime.now()
     with open("data.txt", "a") as myfile:
             myfile.write(str(now.year) + '-' + str(now.month) + '-' + str(now.day) + ';' + str(price) + "\n")
 # If No
 elif new_number_choice == "n":
-    with open("data.txt", 'rb') as fh:
-        for line in fh:
-            pass
-            price = line[11:-2]
+    with open("data.txt") as f:
+        lines=f.readlines()
+        price = float(lines[-1][11:-1])
+        previous_price = float(lines[-2][11:-1])
 with open("data.txt") as f:
     content = [float(x.strip('\n')[11:]) for x in f.readlines()]
 average = sum(content) / len(content)
@@ -33,15 +36,16 @@ dic = {
 '+'  : b'\xe2\x95\x94',
 '%'  : b'\xe2\x95\x97',
 }
-
+percentage_diff = round((previous_price - price) / previous_price * 100, 2)
+percentage_average_diff = round((average - price) / average * 100, 2)
 def decode(x):
     return (''.join(dic.get(i, i.encode('utf-8')).decode('utf-8') for i in x))
 
 print(decode('+------------------------------------%'))
-print(decode('|       Best Price of the Day        |') + "\tinstant-gaming: " + price)
-print(decode('|               ' + str(price) + '                |'))
-print(decode('\\------------------------------------/'))
+print(decode('|       Best Price of the Day        |') + "\tinstant-gaming: " + str(price))
+print(decode('|               ' + str(price) + '                |') + "\tPrevious: " + str(previous_price))
+print(decode('\\------------------------------------/') + "\tDifference: %.2f" % (percentage_diff) + '%')
 print(decode('+------------------------------------%'))
 print(decode('|        Average of Prices           |') + "\tinstant-gaming: " + str(round(average, 2)))
-print(decode('|               ' + str(round(average, 2)) + '                |'))
+print(decode('|               ' + str(round(average, 2)) + '                |') + "\tDifference: %.2f" % (percentage_average_diff) + '%')
 print(decode('\\------------------------------------/'))
