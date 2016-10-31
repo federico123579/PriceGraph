fd13e4acf3d6dd4b9bd3197e6370e7c38a1599be
federico123579 <federico123579@gmail.com>
2016-10-31 10:50:53 +0100

    moved to euros

--- data_processor.py
+++ data_processor.py
@@ -2,7 +2,7 @@ import urllib, json, os, sys
 from pprint import pprint
 import datetime
 # Collect Data
-url = "https://extraction.import.io/query/extractor/d00472f3-457e-4b37-b710-4002797e3087?_apikey=ab96cf38f5fe40e69b3ae0a59c9a2711840b0bee59bd951f25b9a2ee4e2c9d05e7e5e29ae414029bf21cbc3f161452143630d8125cf5572ebcb9b321ffd4ac087ef33b9bcf6ad7330418ebdb43e2243e&url=https%3A%2F%2Fwww.instant-gaming.com%2Fit%2F1421-comprare-key-origin-battlefield-1%2F"
+url = "https://extraction.import.io/query/extractor/518ceba2-e6e0-46d5-b607-170fb36c5108?_apikey=ab96cf38f5fe40e69b3ae0a59c9a2711840b0bee59bd951f25b9a2ee4e2c9d05e7e5e29ae414029bf21cbc3f161452143630d8125cf5572ebcb9b321ffd4ac087ef33b9bcf6ad7330418ebdb43e2243e&url=https%3A%2F%2Fwww.instant-gaming.com%2Fit%2F1421-comprare-key-origin-battlefield-1%2F%3Fcurrency%3DEUR"
 now = datetime.datetime.now()
 with open("data.txt") as f:
     lines = f.readlines()
@@ -15,7 +15,7 @@ with open("data.txt") as f:
         response = urllib.urlopen(url)
         data = json.loads(response.read())
         price_str = json.dumps(data["extractorData"]["data"][0]["group"][0]["Price"][0]["text"])
-        price = float(price_str[2:-1])
+        price = float(price_str[1:-7])
         previous_price = float(lines[-1][11:-1])
 # Write Data
         with open("data.txt", "a") as f:
@@ -38,12 +38,11 @@ percentage_diff = round((price - previous_price) / previous_price * 100, 2)
 percentage_average_diff = round((price - average) / average * 100, 2)
 def decode(x):
     return (''.join(dic.get(i, i.encode('utf-8')).decode('utf-8') for i in x))
-
 print(decode('+------------------------------------%'))
-print(decode('|           Battlefield 1            |') + "\tinstant-gaming: $%.2f" % price)
-print(decode('|              ') + "$%.2f" % price + decode('                |') + "\tPrevious: %.2f" % previous_price)
+print(decode('|           Battlefield 1            |') + "\tinstant-gaming: %.2f" % price)
+print(decode('|               ') + "%.2f" % price + decode('                |') + "\tPrevious: %.2f" % previous_price)
 print(decode('\\------------------------------------/') + "\tDifference: %.2f" % (percentage_diff) + '%')
 print(decode('+------------------------------------%'))
-print(decode('|        Average of Prices           |') + "\tinstant-gaming: $%.2f" % round(average, 2))
-print(decode('|              ') + "$%.2f" % round(average, 2) + decode('                |') + "\tDifference: %.2f" % (percentage_average_diff) + '%')
+print(decode('|        Average of Prices           |') + "\tinstant-gaming: " + "%.2f" % round(average, 2))
+print(decode('|               ') + "%.2f" % round(average, 2) + decode('                |') + "\tDifference: %.2f" % (percentage_average_diff) + '%')
 print(decode('\\------------------------------------/'))
