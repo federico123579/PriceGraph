c86b255da69cb8e7a693ce147f45af7a08f34907
PcFedeUbuntu <federico123579@gmail.com>
2016-11-03 21:17:13 +0100

    made a new module from import data

--- Modules/priceExtractor.py
+++ Modules/priceExtractor.py
@@ -1,12 +1,7 @@
-'''import urllib2
-import xml.etree.ElementTree as ET
-# Not Module
-url = "https://www.instant-gaming.com/it/1421-comprare-key-origin-battlefield-1/"
-req = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
-html = urllib2.urlopen(req).read()
-with open("response.html", "w") as f:
-    f.write(html)
-root = ET.parse('response.html')
-root.findall('//*[@id="ig-product-main-panel-boxprice-contentright-price]')'''
-from xml import html
+from lxml import html
 import requests
+url = "https://www.instant-gaming.com/it/1421-comprare-key-origin-battlefield-1/"
+page = requests.get(url)
+tree = html.fromstring(page.content)
+price = tree.xpath('//span[@id="ig-product-main-panel-boxprice-contentright-price"]/text()')
+print price[0][:5]
