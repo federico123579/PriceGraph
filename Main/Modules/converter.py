1e5c2e0599b1c546bc119e8270c5ca23fd15215e
federico123579 <federico123579@gmail.com>
2016-11-05 23:25:05 +0100

    resolved bugs

--- Main/Modules/converter.py
+++ Main/Modules/converter.py
@@ -1,10 +1,10 @@
 import fileinput, library
-def convert():
-    file_data = fileinput.input("../data.txt", inplace=True)
+def convert(link_to_file):
+    file_data = fileinput.input(link_to_file, inplace=True)
     new_line = 'Date'
     for x in range(len(library.library)):
         new_line = new_line + ',' + library.library[x]['name']
-    with open('../data.txt') as f:
+    with open(link_to_file) as f:
         line_test = f.readline()
         print line_test
     for line in file_data:
@@ -12,9 +12,10 @@ def convert():
             print new_line
         else:
             print line.strip('\n')
-def delete_last_line():
-    file_data = fileinput.input("../data.txt", inplace=True)
-    last_line = f.readlines()[-1][11:-1]
+def delete_last_line(link_to_file):
+    file_data = fileinput.input(link_to_file, inplace=True)
+    with open(link_to_file) as f:
+        last_line = f.readlines()[-1][11:-1]
     for line in file_data:
         if last_line == line:
             print ''
@@ -22,4 +23,4 @@ def delete_last_line():
             print line.strip('\n')
 
 if __name__ == '__main__':
-    convert()
+    convert("../data.txt")
--- Main/Modules/dataProcessor.py
+++ Main/Modules/dataProcessor.py
@@ -3,9 +3,9 @@ import  datetime, schedule, sys, time
 import priceExtractor as pe
 import library as lib
 # Collect Data
-def collect_data():
+def collect_data(link_to_file):
     now = datetime.datetime.now()
-    with open("../data.txt") as f:
+    with open(link_to_file) as f:
         lines = f.readlines()
         # If Yes
         if int(lines[-1][8:10]) == int(now.day):
@@ -19,9 +19,9 @@ def collect_data():
                     price = str(price) + ',' + str(pe.import_price(lib.library[x]['url']))
 
             # Write Data
-            with open("../data.txt", "a") as f:
+            with open(link_to_file, "a") as f:
                 f.write('%02d' % now.year + '-' + '%02d' % now.month + '-' + '%02d' % now.day + ',' + price + "\n")
 
 if __name__ == '__main__':
     #schedule.every().day.at("10:30").do(job)
-    collect_data()
+    collect_data('../data.txt')
