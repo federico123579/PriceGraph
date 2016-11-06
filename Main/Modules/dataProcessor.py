9dcab38d26c48f541abf2b14c77b231fdab9a6e1
federico123579 <federico123579@gmail.com>
2016-11-06 10:15:30 +0100

    refined

--- Main/Modules/dataProcessor.py
+++ Main/Modules/dataProcessor.py
@@ -2,6 +2,7 @@ import  datetime, schedule, sys, time
 #import schedule
 import priceExtractor as pe
 import library as lib
+import converter
 # Collect Data
 def collect_data(link_to_file):
     now = datetime.datetime.now()
@@ -9,18 +10,19 @@ def collect_data(link_to_file):
         lines = f.readlines()
         # If Yes
         if int(lines[-1][8:10]) == int(now.day):
-            pass
+            converter.delete_last_line('data.txt')
         # If No
         else:
-            for x in range(len(lib.library)):
-                if x == 0:
-                    price = pe.import_price(lib.library[x]['url'])
-                else:
-                    price = str(price) + ',' + str(pe.import_price(lib.library[x]['url']))
+            pass
+        for x in range(len(lib.library)):
+            if x == 0:
+                price = pe.import_price(lib.library[x]['url'])
+            else:
+                price = str(price) + ',' + str(pe.import_price(lib.library[x]['url']))
 
-            # Write Data
-            with open(link_to_file, "a") as f:
-                f.write('%02d' % now.year + '-' + '%02d' % now.month + '-' + '%02d' % now.day + ',' + price + "\n")
+        # Write Data
+        with open(link_to_file, "a") as f:
+            f.write('%02d' % now.year + '-' + '%02d' % now.month + '-' + '%02d' % now.day + ',' + price + "\n")
 
 if __name__ == '__main__':
     #schedule.every().day.at("10:30").do(job)
--- Main/PriceGraph.py
+++ Main/PriceGraph.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(0, 'Modules')
-import librarian, dataProcessor, grapher, converter
+import librarian, dataProcessor, grapher
 new_videogame_option = input("New Videogame?(1/0)")
 if new_videogame_option == 1:
     name = raw_input("What's its name?")
@@ -8,9 +8,7 @@ if new_videogame_option == 1:
     url = raw_input("What's its url?")
     librarian.new_game(name, price, url, 'data.txt')
     dataProcessor.collect_data('data.txt')
-    converter.delete_last_line('data.txt')
 else:
     pass
-converter.delete_last_line('data.txt')
 dataProcessor.collect_data('data.txt')
 grapher.send_to_make_graph()
